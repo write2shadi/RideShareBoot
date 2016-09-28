f405d20c9a5dcb298f2f94a00f56290401b515a8
hongyanqu <hongyan.qu.8@gmail.com>
2016-09-27 19:24:37 -0500

    using current user identity for rideinfo

--- RideShareBoot/src/main/java/mum/ea/service/RideInfoServiceImpl.java
+++ RideShareBoot/src/main/java/mum/ea/service/RideInfoServiceImpl.java
@@ -4,21 +4,27 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import mum.ea.domain.CurrentUser;
 import mum.ea.domain.RideInfo;
+import mum.ea.domain.User;
 import mum.ea.repository.RideInfoRepository;
 
 @Service
 public class RideInfoServiceImpl implements RideInfoService {
 
   private final RideInfoRepository rideInfoRepository;
-
+ 
     @Autowired
     public RideInfoServiceImpl(RideInfoRepository rideInfoRepository) {
         this.rideInfoRepository = rideInfoRepository;
+        
     }
+        
 
 	public List<RideInfo> getAllRideInfos() {
 		return rideInfoRepository.findAll();
@@ -28,7 +34,8 @@ public class RideInfoServiceImpl implements RideInfoService {
 
 	public void addRideInfo(RideInfo rideinfo) {
 		
-		   
+		User u =  ((CurrentUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser() ;
+		rideinfo.setUser(u);
 		rideInfoRepository.save(rideinfo);
 	     
 
